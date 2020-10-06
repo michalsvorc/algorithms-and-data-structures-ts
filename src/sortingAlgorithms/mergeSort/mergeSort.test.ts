627a067c227f0ce464946019fae70da1471797b9
Michal Svorc <mail@michalsvorc.com>
2020-10-06 12:12:31 +0000

    Normalize sorting tests, test input array mutation

--- src/sortingAlgorithms/mergeSort/mergeSort.test.ts
+++ src/sortingAlgorithms/mergeSort/mergeSort.test.ts
@@ -1,26 +1,32 @@
-import mergeSort from './mergeSort';
+import sortingAlgorithm from './mergeSort';
 
 const sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-const shuffledArray = [3, 5, 1, 4, 6, 8, 10, 9, 7, 2];
+const unsortedArray = [3, 5, 1, 4, 6, 8, 10, 9, 7, 2];
+const unsortedArrayOriginal = Object.freeze([...unsortedArray]);
 
 describe('Merge sort', () => {
   test('empty array', () => {
-    expect(mergeSort([])).toEqual([]);
+    expect(sortingAlgorithm([])).toEqual([]);
   });
 
   test('array with one element', () => {
-    expect(mergeSort([1])).toEqual([1]);
+    expect(sortingAlgorithm([1])).toEqual([1]);
   });
 
   test('odd unsorted array', () => {
     const filterReducer = (n) => n < 10;
-    expect(mergeSort(shuffledArray.filter(filterReducer))).toEqual(
+
+    expect(sortingAlgorithm(unsortedArray.filter(filterReducer))).toEqual(
       sortedArray.filter(filterReducer)
     );
   });
 
   test('even unsorted array', () => {
-    expect(mergeSort(shuffledArray)).toEqual(sortedArray);
+    expect(sortingAlgorithm(unsortedArray)).toEqual(sortedArray);
+  });
+
+  test('input array is not mutated', () => {
+    sortingAlgorithm(unsortedArray);
+    expect(unsortedArray).toEqual(unsortedArrayOriginal);
   });
 });
--- src/sortingAlgorithms/quickSort/quickSort.test.ts
+++ src/sortingAlgorithms/quickSort/quickSort.test.ts
@@ -1,26 +1,32 @@
-import quickSort from './quickSort';
+import sortingAlgorithm from './quickSort';
 
 const sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-const shuffledArray = [3, 5, 1, 4, 6, 8, 10, 9, 7, 2];
+const unsortedArray = [3, 5, 1, 4, 6, 8, 10, 9, 7, 2];
+const unsortedArrayOriginal = Object.freeze([...unsortedArray]);
 
 describe('Quicksort', () => {
   test('empty array', () => {
-    expect(quickSort([])).toEqual([]);
+    expect(sortingAlgorithm([])).toEqual([]);
   });
 
   test('array with one element', () => {
-    expect(quickSort([1])).toEqual([1]);
+    expect(sortingAlgorithm([1])).toEqual([1]);
   });
 
   test('odd unsorted array', () => {
     const filterReducer = (n) => n < 10;
-    expect(quickSort(shuffledArray.filter(filterReducer))).toEqual(
+
+    expect(sortingAlgorithm(unsortedArray.filter(filterReducer))).toEqual(
       sortedArray.filter(filterReducer)
     );
   });
 
   test('even unsorted array', () => {
-    expect(quickSort(shuffledArray)).toEqual(sortedArray);
+    expect(sortingAlgorithm(unsortedArray)).toEqual(sortedArray);
+  });
+
+  test('input array is not mutated', () => {
+    sortingAlgorithm(unsortedArray);
+    expect(unsortedArray).toEqual(unsortedArrayOriginal);
   });
 });
