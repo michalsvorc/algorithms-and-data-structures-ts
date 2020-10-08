8bc6300b897cf638cec875c856f20adf29130f36
Michal Svorc <mail@michalsvorc.com>
2020-10-08 14:26:47 +0000

    Add BST inOrder traversal an isEmpty methods

--- src/dataStructures/binarySearchTree/BinarySearchTree.test.ts
+++ src/dataStructures/binarySearchTree/BinarySearchTree.test.ts
@@ -6,6 +6,8 @@ describe('BinarySearchTree', () => {
 
     expect(bst).toBeDefined();
     expect(bst.root).toBeNull();
+    expect(bst.isEmpty()).toBeTruthy();
+    expect(bst.inOrderRepresentation).toBeNull();
   });
 
   describe('should insert', () => {
@@ -13,56 +15,38 @@ describe('BinarySearchTree', () => {
       const bst = new BinarySearchTree();
 
       bst.insert(10);
-      expect(bst.root.data).toBe(10);
-      expect(bst.root.left).toBeNull;
-      expect(bst.root.right).toBeNull;
+      expect(bst.root?.data).toBe(10);
+      expect(bst.inOrderRepresentation).toBe('10');
     });
 
     it('nodes to create balanced tree', () => {
       const bst = new BinarySearchTree();
 
       bst.insert(20);
-      const insertedNode1 = bst.insert(10);
-      const insertedNode2 = bst.insert(30);
+      bst.insert(10);
+      bst.insert(30);
 
-      const leftChild = bst.root.left;
-      const rightChild = bst.root.right;
-
-      expect(leftChild?.data).toBe(insertedNode1.data);
-      expect(rightChild?.data).toBe(insertedNode2.data);
-
-      expect(leftChild?.left).toBeNull;
-      expect(leftChild?.right).toBeNull;
-      expect(rightChild?.left).toBeNull;
-      expect(rightChild?.right).toBeNull;
+      expect(bst.inOrderRepresentation).toBe('10,20,30');
     });
 
     it('nodes to create left leafed tree', () => {
       const bst = new BinarySearchTree();
 
       bst.insert(30);
-      const insertedNode1 = bst.insert(20);
-      const insertedNode2 = bst.insert(10);
+      bst.insert(20);
+      bst.insert(10);
 
-      expect(bst.root?.left?.data).toBe(insertedNode1.data);
-      expect(bst.root?.left?.left?.data).toBe(insertedNode2.data);
-
-      expect(bst.root.right).toBeNull;
-      expect(bst.root?.left?.right).toBeNull;
+      expect(bst.inOrderRepresentation).toBe('10,20,30');
     });
 
     it('nodes to create right leafed tree', () => {
       const bst = new BinarySearchTree();
 
       bst.insert(10);
-      const insertedNode1 = bst.insert(20);
-      const insertedNode2 = bst.insert(30);
+      bst.insert(20);
+      bst.insert(30);
 
-      expect(bst.root?.right?.data).toBe(insertedNode1.data);
-      expect(bst.root?.right?.right?.data).toBe(insertedNode2.data);
-
-      expect(bst.root.left).toBeNull;
-      expect(bst.root?.right?.left).toBeNull;
+      expect(bst.inOrderRepresentation).toBe('10,20,30');
     });
   });
 });
--- src/dataStructures/binarySearchTree/BinarySearchTree.ts
+++ src/dataStructures/binarySearchTree/BinarySearchTree.ts
@@ -1,11 +1,23 @@
 import BinaryTreeNode, {IBinaryTreeNode} from './BinaryTreeNode';
 
+interface IBinarySearchTree {
+  isEmpty(): boolean;
+  insert(data: number): IBinaryTreeNode;
+  root: IBinaryTreeNode | null;
+  inOrderRepresentation: string | null;
+}
+
 /** Class declaration for constructing a Binary search tree (BST) data structure. */
-export default class BinarySearchTree {
+export default class BinarySearchTree implements IBinarySearchTree {
   #root;
 
   constructor() {
     this.#root = null;
+  }
+
+  /** Returns a Boolean value indicating whether the BST has any nodes. */
+  isEmpty(): boolean {
+    return this.#root === null;
   }
 
   /** Insert new node to BST. */
@@ -13,7 +25,7 @@ export default class BinarySearchTree {
     const newNode = new BinaryTreeNode(data);
 
     /** If BST is empty, insert new node as root. */
-    if (this.#root === null) {
+    if (this.isEmpty()) {
       this.#root = newNode;
       return this.#root;
     }
@@ -51,8 +63,39 @@ export default class BinarySearchTree {
     return this.insertNode(currentNode.right, newNode);
   }
 
+  /**
+   * Perform inOrder traversal of a tree starting from a given node
+   * 1. Traverse the left subtree i.e perform inOrder on left subtree
+   * 2. Visit the root
+   * 3. Traverse the right subtree i.e perform inOrder on right subtree
+   */
+  private traverseInOrder(node: IBinaryTreeNode): number[] {
+    let traversalList: number[] = [];
+
+    /** Add left node of current traversal level. */
+    if (node.left) {
+      traversalList = traversalList.concat(this.traverseInOrder(node.left));
+    }
+
+    /** Add root of current traversal level. */
+    traversalList.push(node.data);
+
+    /** Add right node of current traversal level. */
+    if (node.right) {
+      traversalList = traversalList.concat(this.traverseInOrder(node.right));
+    }
+
+    return traversalList;
+  }
+
   /** Gets the root node of BST. */
-  get root(): IBinaryTreeNode {
+  get root(): IBinaryTreeNode | null {
     return this.#root;
+  }
+
+  /** Gets string representation of BST in order traversal. */
+  get inOrderRepresentation(): string | null {
+    if (this.isEmpty()) return null;
+    return this.traverseInOrder(this.#root).toString();
   }
 }
